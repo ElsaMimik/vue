3a6a5d9e46f1e9b6de68d5df8ba4f538844e61c7
silencechung <lalpha922@gmail.com>
2017-12-18 16:19:43 +0800

    honor commit again

--- Vue-Testing-DotNet-3/Vue-Testing-DotNet-3/Scripts/src/home/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import Welcome from '@/components/Welcome.vue';
-new Vue({
-    el: '#app',
-    template: '<Welcome/>',
-    components: { Welcome: Welcome }
-});
-//# sourceMappingURL=index.js.map
