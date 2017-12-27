3088404be56a26c3ad1fb62f16eced113064628a
CHING-CHUN CHEN <alpha.chen@higgstar.com>
2017-12-27 10:31:26 +0800

    fix wrong init command

--- Vue-Testing-DotNet-3/Vue-Testing-DotNet-3/buildenv.ps1
+++ Vue-Testing-DotNet-3/Vue-Testing-DotNet-3/buildenv.ps1
@@ -6,15 +6,16 @@ else {
     Start-Process powershell -Verb runAs -WorkingDirectory (Get-Location).Path -Wait -ArgumentList $chocoInstallCommand;
 }
 
+$nodeInstallCommand = "/c choco install netfx-4.7.1-devpack -y";
+Start-Process powershell -Verb runAs -WorkingDirectory (Get-Location).Path -Wait -ArgumentList $nodeInstallCommand
+
 if (Get-Command "npm" -errorAction SilentlyContinue) {
     "node exist, pass install"
 }
 else {
-    $nodeInstallCommand = "/c choco install netfx-4.7.1-devpack -y";
-    Start-Process powershell -Verb runAs -WorkingDirectory (Get-Location).Path -Wait -ArgumentList $nodeInstallCommand
+    Start-Process powershell -Verb runAs -WorkingDirectory (Get-Location).Path -Wait -ArgumentList "/c choco install nodejs --version 9.3.0 -n -y"
 }
 
-Start-Process powershell -Verb runAs -WorkingDirectory (Get-Location).Path -Wait -ArgumentList "/c choco install nodejs --version 9.3.0 -n -y"
 Start-Process powershell -Verb runAs -WorkingDirectory (Get-Location).Path -Wait -ArgumentList "/c npm install webpack -g"
 Start-Process powershell -Verb runAs -WorkingDirectory (Get-Location).Path -Wait -ArgumentList "/c npm install webpack-dev-server -g"
 Start-Process powershell -Verb runAs -WorkingDirectory (Get-Location).Path -Wait -ArgumentList "/c npm install"
